a299164e911932d8787ec83ed9d46bc3ed887e6e
sekom89 <sekom89@gmail.com>
2022-04-28 14:01:16 +0900

    원형 물결 알고리즘
    한점 중심으로, 물결 모양으로 원이 생성되고, 일정 값으로 각각 포지션들을 얻을 수 있다.
    1

---
+++ CircleDistributionScript.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleDistributionScript : MonoBehaviour {
+
+    public int CircleCount;
+    public float Radius;
+    private float thetaScale = 0.25f;
+    
+    public GameObject PlayerPrefab;
+    public GameObject DrawPrefab;
+    
+    private GameObject basePoint;
+    private List<GameObject> prefabList = new List<GameObject>();
+    private List<DrawRadar> DrawRadarList = new List<DrawRadar>();
+    
+    void Start() {
+
+        prefabList.Clear();
+        DrawRadarList.Clear();
+        CirlceDrawInit();
+        PointInit();
+    }
+
+    private void CirlceDrawInit() {
+
+        int count = (int) Mathf.Log(CircleCount, 2) - 1;
+        if (count < 1) {
+            count = 1;
+        }
+        
+
+        for (int i = 0; i < count; i++) {
+            var obj = GameObject.Instantiate(DrawPrefab);
+            obj.transform.parent = this.transform;
+            var radar = obj.GetComponent<DrawRadar>();
+            
+            radar.Init(thetaScale / (i+1), Radius * (i+1));
+            DrawRadarList.Add(radar);
+        }
+    }
+
+    private void PointInit() {
+        
+
+        if (CircleCount == 1) {
+            var obj = GameObject.Instantiate(PlayerPrefab);
+            basePoint = this.transform.Find("BasePoint").gameObject;
+            obj.transform.position = basePoint.transform.position;
+            prefabList.Add(obj);
+            return;
+        }
+        
+        List<Vector3> randomPointList = null;
+
+        int prevCount = 0;
+        randomPointList = DrawRadarList[prevCount].GetRandomArrayPoint();
+
+        int Arrayindex = 0;
+        for (int i = 0; i < CircleCount; i++) {
+            var obj = GameObject.Instantiate(PlayerPrefab);
+            
+            if (randomPointList.Count == Arrayindex) {
+
+                prevCount++;
+                if (DrawRadarList.Count == prevCount) {
+                    break;
+                }
+                randomPointList = DrawRadarList[prevCount].GetRandomArrayPoint();
+                Arrayindex = 0;
+            }
+            
+            Debug.Log($" CircleCount : {i}");
+            obj.name = i.ToString();
+            obj.transform.position = randomPointList[Arrayindex++];
+            prefabList.Add(obj);
+        }
+    }
+    
+}
+
---
+++ DrawRadar.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DrawRadar : MonoBehaviour
+{
+    private float ThetaScale = 0.25f;
+    private float Radius = 0.5f;
+    private int Size;
+    private LineRenderer LineDrawer;
+    private float Theta = 0f;
+    private List<Vector3> point = new List<Vector3>();
+
+    public void Init(float thetaScale, float radius ) {
+        ThetaScale = thetaScale;
+        Radius = radius;
+        LineDrawer = GetComponent<LineRenderer>();
+        PosUpdate();
+    }
+
+    void PosUpdate() {
+        Theta = 0f;
+        Size = (int)((1f / ThetaScale) + 1f);
+        LineDrawer.SetVertexCount(Size);
+        for (int i = 0; i < Size; i++) {
+            Theta += (2.0f * Mathf.PI * ThetaScale);
+            float x = Radius * Mathf.Cos(Theta);
+            float y = Radius * Mathf.Sin(Theta);
+            point.Add(new Vector3(x, 0, y));
+            LineDrawer.SetPosition(i, point[i]);
+        }
+    }
+
+    public List<Vector3> GetRandomArrayPoint() {
+        
+        //끝에 포인트는 같은 위치라 지움
+        point.RemoveAt(point.Count - 1);
+
+        for (int i = point.Count - 1;  i > 0; i--) {
+            int rnd = UnityEngine.Random.Range(0, i);
+            Vector3 temp = point[i];
+            point[i] = point[rnd];
+            point[rnd] = temp;
+        }
+
+        return point;
+    }
+}
